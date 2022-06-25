23e9f8a0e79fc0ac1b0cd458a649e3c574adf7a3
yangjun2580 <yangjun2580@naver.com>
2022-06-25 11:48:55 +0900

    :green_heart: seoyoung

--- Seoyoung/settings.py
+++ Seoyoung/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'Seoyoung.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seoyoung1',
+        'USER': 'root',
+        'PASSWORD': 'Yy960506!@@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -143,5 +146,5 @@ MEDIA_URL = '/media/' # 항상 / 로 끝나도록 설정
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # 커스텀 유저 모델 사용
-AUTH_USER_MODEL = 'user.User'
+
 

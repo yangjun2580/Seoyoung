6b4c529b249c512137bbada9f48ac400f572c881
yangjun2580 <yangjun2580@naver.com>
2023-10-10 19:15:41 +0900

    :green_heart: seoyoung

--- content/views.py
+++ content/views.py
@@ -38,8 +38,7 @@ class Main(APIView):
                 image=feed.image,
                 content=feed.content,
                 like_count=like_count,
-                profile_image=user.profile_image,
-                nickname=user.nickname,
+                profile_image=user.profile_image, nickname=user.nickname,
                 reply_list=reply_list,
                 is_liked=is_liked,
                 is_marked=is_marked
